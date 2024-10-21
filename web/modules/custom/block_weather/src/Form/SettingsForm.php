1a3b4e2a87649e68ea6b81f0f9f91c9538f8190b
keopx <keopx@keopx.net>
2024-10-21 18:14:34 +0200

    Issue: #3374955: Use config_target for configruation form

--- web/modules/custom/block_weather/src/Form/SettingsForm.php
+++ web/modules/custom/block_weather/src/Form/SettingsForm.php
@@ -12,10 +12,8 @@ class SettingsForm extends ConfigFormBase {
 
   /**
    * Config settings.
-   *
-   * @var string
    */
-  const SETTINGS = 'block_weather.settings';
+  const string SETTINGS = 'block_weather.settings';
 
   /**
    * {@inheritdoc}
@@ -43,28 +41,16 @@ class SettingsForm extends ConfigFormBase {
       '#type' => 'key_select',
       '#title' => $this->t('API Key'),
       '#description' => $this->t('The API key to authenticate with the Weather API.'),
-      '#default_value' => $config->get('api_key'),
+      '#config_target' => 'block_weather.settings:api_key',
     ];
 
     $form['city'] = [
       '#type' => 'textfield',
       '#title' => $this->t('City'),
-      '#default_value' => $config->get('city'),
+      '#config_target' => 'block_weather.settings:city',
     ];
 
     return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config(static::SETTINGS)
-      ->set('api_key', $form_state->getValue('api_key'))
-      ->set('city', $form_state->getValue('city'))
-      ->save();
-
-    parent::submitForm($form, $form_state);
   }
 
 }
